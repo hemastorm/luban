356cacc6e4657bab56f7ab757eaab54fc09f8a80
hemastorm <xiawei13@126.com>
2018-09-02 23:59:08 +0800

    git

--- src/main/java/com/storm/luban/ctrl/MaterialCtrl.java
+++
@@ -1,9 +0,0 @@
-package com.storm.luban.ctrl;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-@ResponseBody
-public class MaterialCtrl {
-}
